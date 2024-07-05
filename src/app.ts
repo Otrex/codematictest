d921870f23a8fd245643747d63a431edcde64f5a
Treasure O <treasure@tronweb.co>
2024-07-05 14:14:31 +0100

    feat: added the endpoints

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/', (_, res) => res.send('Hello World!'))
 app.use('/api', router)
 
 app.use(errorHandler)
